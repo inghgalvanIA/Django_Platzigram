b6a33ac98b001367765ae066e5a6e137f66e1665
Hector Galvan <inghgalvan@gmail.com>
2021-05-17 21:12:06 -0500

    extencion del modelo de usuario

--- platzigram/posts/models.py
+++ platzigram/posts/models.py
@@ -1,18 +1,24 @@
 from django.db import models
 
-# Create your models here.
-class User(models.Model):
-    """User model."""
+# # Create your models here.
+# class User(models.Model):
+#     """User model."""
 
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
+#     email = models.EmailField(unique=True)
+#     password = models.CharField(max_length=100)
 
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+#     first_name = models.CharField(max_length=100)
+#     last_name = models.CharField(max_length=100)
 
-    bio = models.TextField(blank=True)
+#     is_admin = models.BooleanField(default=False)
 
-    birthdate = models.DateField(blank=True, null=True)
+#     bio = models.TextField(blank=True)
 
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
+#     birthdate = models.DateField(blank=True, null=True)
+
+#     created = models.DateTimeField(auto_now_add=True)
+#     modified = models.DateTimeField(auto_now=True)
+
+#     def __str__(self):
+#         """Return email."""
+#         return self.email
